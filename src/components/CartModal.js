9d6a208794c6fe8f1c0e989fdbb0bc3ae9c2185d
Ashutosh Mishra <ashutosh.shai84@gmail.com>
2022-10-14 05:20:53 +0000

    Update CartModal.js

--- src/components/CartModal.js
+++ src/components/CartModal.js
@@ -2,20 +2,57 @@ import React, { useState, useContext } from "react";
 import AppContext from "../context/myntraContext";
 
 function CartModal(props) {
-    const {cart}= useContext(AppContext)
-    
-    return (
-       <h1>Hello1 cart</h1>
+  const { cart } = useContext(AppContext);
+  console.log("---", cart);
+
+  return (
+    <div
+      id="cart-modal-content"
+      style={{
+        position: "absolute",
+        zIndex: 1000,
+        top: "45%",
+        left: "30%",
+        background: "white",
+        padding: 10,
+      }}
+    >
+      <div
+        onClick={() => {
+          props.onClose();
+        }}
+      >
+        X
+      </div>
+      {cart.map((items) => {
+        return (
+          <div
+            className="indiv-tile-holder"
+            style={{ marginRight: 20, marginBottom: 20 }}
+          >
+            <img
+              src={items.product.otherImages[0]}
+              width="100px"
+              height="100px"
+            ></img>
+
+            <div>{items.product.name}</div>
+            <div>
+              <i>{items.product.description}</i>
+            </div>
+            <div>Rs.{items.product.finalPrice}</div>
+            <div>
+              <strike>{items.product.strickPrice}</strike>
+            </div>
+            <div style={{ color: "red" }}>{items.product.discount}% OFF</div>
+          </div>
+        );
+      })}
+    </div>
+
     //  <div
-    //   id="cart-modal-content"      
-    //   style={{
-    //     position: "absolute",
-    //     zIndex: 1000,
-    //     top: "45%",
-    //     left: "30%",
-    //     background: "white",
-    //     padding: 10,
-    //   }}
+    //   id="cart-modal-content"
+
     // >
     //   <div
     //     style={{
@@ -33,36 +70,24 @@ function CartModal(props) {
     //       X
     //     </div>
     //   </div>
-      
+
     //   <div>
-          
+
     //       {
     //           cart.map((items)=>{
-                  
+
     //               return (
     //                   <div>
     //                       <div>
-    //                       <img src={items.products.otherImages[0]}></img>
-    //                       </div>
-    //                       <div>{items.products.name}</div>
-    //                         <div>
-    //                         <i>{items.products.description}</i>
-    //                         </div>
-    //                         <div>Rs.{items.products.finalPrice}</div>
-    //                         <div>
-    //                         <strike>{items.products.strickPrice}</strike>
-    //                         </div>
-    //                         <div style={{ color: "red" }}>{items.products.discount}% OFF</div>   
-    //                     </div>
-                      
+    // </div>
+
     //               )
-                  
+
     //           })
     //       }
-          
+
     //   </div>
-      
-      )  
-     }
+  );
+}
 
 export default CartModal;
